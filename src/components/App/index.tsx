8cae9885cb156f78896ac8badaaf6314bbfe021e
Henry Li <henry1943@163.com>
2019-06-28 15:09:08 +0800

    Update index.tsx

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 
 export const App = () => {
-  const [a] = React.useState(0);
-  if (a === 0) {
-    throw new Error('Fuck');
-  }
   return (
     <div>
       <h1>Hello, world</h1>
